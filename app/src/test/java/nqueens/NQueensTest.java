eca72496eed03d15d099300511fc93102f4deac6
Mark Kenneth Ranosa <ken.ranosa@gmail.com>
2020-12-12 17:34:40 +0800

    NQueens integ test plan draft

---
+++ app/src/test/java/nqueens/NQueensTest.java
@@ -0,0 +1,44 @@
+package nqueens;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class NQueensTest {
+
+	@Test
+	public void test_input() {
+		// TODO should return 0 solution when input is less than 1
+
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void test_specifications() {
+		// TODO should have correct number of queens in board
+		// TODO should have no two queens in the same row
+		// TODO should have no two queens in the same column
+		// TODO should have no two queens diagonal each other
+
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void test_correct_solutions_count() {
+		// TODO should have correct number of solutions per number of queens
+
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void test_correct_solutions() {
+		// TODO [[0:5], [1:3], [2:0], [3:4], [4:7], [5:1], [6:6], [7:2]] in 8
+		// TODO 0° and mirrored solution should exist
+		// TODO 90° and mirrored solution should exist
+		// TODO 180° and mirrored solution should exist
+		// TODO 270° and mirrored solution should exist
+
+		fail("Not yet implemented");
+	}
+
+}
